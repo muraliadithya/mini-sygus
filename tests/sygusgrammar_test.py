aa92cc516ee311bffc308c9db0e92092b5c3eee7
muraliadithya <adithyamuralism@gmail.com>
2020-12-29 05:25:18 -0600

    Adding simple test to show usage of SyGusGrammar class

---
+++ tests/sygusgrammar_test.py
@@ -0,0 +1,28 @@
+from src.SyGuSGrammar import parse_synthfun_command
+
+synthfun_str = """
+(synth-fun lemma ((x Int) (y (Set Int))  ) Bool
+           ((Start Bool) (B1 Bool) (B2 Bool) (B3 Bool) (Loc Int))
+
+           ((Start Bool (
+                  (=> B1 (and B2 B3))))
+           (B1 Bool
+                  ((member Loc (hbst Loc))))
+          (B2 Bool
+                  ((<= (key Loc) (maxr Loc))))
+           (B3 Bool
+                  ((<= (minr Loc) (key Loc))))
+           (Loc Int (x
+                     y))
+
+))
+"""
+
+grammar = parse_synthfun_command(synthfun_str)
+for key in grammar.__dict__:
+    print('{}: {}'.format(key, getattr(grammar, key)))
+
+for key in grammar.nonterminals:
+    print('{}:'.format(key))
+    for rule in grammar.rules[key]:
+        print(rule)
