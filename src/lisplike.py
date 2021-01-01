f2c13d5a2c579bf6e57b568d9d3e9c5111aaa334
muraliadithya <adithyamuralism@gmail.com>
2021-01-01 09:30:22 -0600

    Cleanup lisplike module. Adding comparison operation.

--- src/lisplike.py
+++ src/lisplike.py
@@ -2,7 +2,7 @@
 Module containing general utilities for handling lisp-like data. This is the primary form of representation in the 
 SMT-Lib 2.6 format that specifies the input format of files given to a SMT solvers.  
 
-This module in particular provides support for handling lisp-like strings where utterances are separated by 
+In particular this implementations handles strings where utterances are separated by 
 whitespaces and applications are grouped by parentheses. At the level of abstraction provided by this module, 
 utterances are themselves simply strings with no additional semantic annotations. The grammar describing such 
 'lisp-like' expressions (LL_EXP) is:  
@@ -13,15 +13,32 @@ LL_EXP -> WHITESPACE LL_EXP WHITESPACE
 UTTERANCE -> string  
 WHITESPACE -> any of {'\n', ' ', '\t'}  
 
-The module provides functions for reading such lisp-like strings into a nested list of utterances, as well as other 
-miscellaneous utilities like pretty printers.  
+This module provides a cohesive representation for reading in such lisp-like strings as nested lists of utterances 
+(strings), referred to in the code as 'lisplike_repr'. It also provides functions to perform the parsing of 
+strings into the repr, as well as other miscellaneous utilities like pretty printers.  
 """
 
-# TODO (low): give helpful error messages from parser
+# TODO (low): give helpful error messages from parser about parentheses
 # TODO (low): integrate/replace parser with official sygus parser
 
 
-def parser(input_str, noerr=False):
+class NotLispLikeStringException(Exception):
+    """
+    This exception is raised when a string is expected to be lisp-like (able to be parsed into a lisp-like repr), 
+    but is not.  
+    """
+    pass
+
+
+class NotLispLikeReprException(Exception):
+    """
+    This exception is raised when a value is not a lisp-like repr. A lisp-like repr is either a string (utterance)
+    or a list of lisp-like repr (application expressions).  
+    """
+    pass
+
+
+def parse(input_str, noerr=False):
     """
     Basic lisp-like parser.  
     Setting the noerr argument does not raise an exception, instead behaves like a best-effort parser.  
@@ -32,14 +49,14 @@ def parser(input_str, noerr=False):
     # Preprocess: remove arbitrary whitespaces and replace with single space
     processed_str = ' '.join(input_str.split())
     # Call a nested list parser that returns nested list of strings
-    [nested_list_repr], pos = _parser_aux(processed_str, noerr, len(processed_str))
+    [lisplike_repr], pos = _parse_aux(processed_str, noerr, len(processed_str))
     # The last parsed position must be the last position
     if pos != len(processed_str) - 1 and not noerr:
-        raise ValueError('Input is not lisp-like. Could not parse {}'.format(processed_str[pos+1:]))
-    return nested_list_repr
+        raise NotLispLikeStringException('Input is not lisp-like. Could not parse {}'.format(processed_str[pos + 1:]))
+    return lisplike_repr
 
 
-def _parser_aux(in_str, noerr, end, begin=0):
+def _parse_aux(in_str, noerr, end, begin=0):
     """
     Simple function to parse lisp-like strings where the delimiters are single spaces.  
     The function returns a 'partially' parsed nested list and an integer, where the integer denotes the position 
@@ -70,8 +87,9 @@ def _parser_aux(in_str, noerr, end, begin=0):
             # Recursive call
             # Recursive call can only be made if there are no utterances to be parsed at this point
             if utterance_accumulator != '' and not noerr:
-                raise ValueError('Input is not lisp-like. Utterance {} is not part of any expression.'.format(utterance_accumulator))
-            partial_repr, partial_pos = _parser_aux(in_str, noerr, end=end, begin=curr+1)
+                raise NotLispLikeStringException('Input is not lisp-like. Utterance {} '
+                                                 'is not part of any expression.'.format(utterance_accumulator))
+            partial_repr, partial_pos = _parse_aux(in_str, noerr, end=end, begin=curr+1)
             partial_result.append(partial_repr)
             # Update curr to last parsed position, which is partial_pos
             curr = partial_pos
@@ -108,14 +126,14 @@ def pretty_string(nested_list, noindent=False):
     return result
 
 
-def _pretty_string_aux(nested_list, noindent, align=0):
-    if isinstance(nested_list, str):
+def _pretty_string_aux(lisplike_repr, noindent, align=0):
+    if isinstance(lisplike_repr, str):
         # Single utterance. Just return the string, respecting alignment.
-        result = ' ' * align + nested_list
+        result = ' ' * align + lisplike_repr
         return result
     # Open parentheses, respecting column alignment
     result = ' ' * align + '('
-    operator, *operands = nested_list
+    operator, *operands = lisplike_repr
     operator_str = operator
     if isinstance(operator, list):
         # Directive or declaration. Switch off indentation for the entire expression.
@@ -137,6 +155,20 @@ def _pretty_string_aux(nested_list, noindent, align=0):
 
 
 # Additional utilities for denoting a 'lisp-like' string or nested list
+def is_lisplike(value):
+    """
+    Checking whether a value is a lisp-like representation.  
+    :param value: any  
+    :return: bool  
+    """
+    if isinstance(value, str):
+        return True
+    if isinstance(value, list):
+        return all(is_lisplike(v) for v in value)
+    else:
+        return False
+
+
 def is_lisplike_string(input_str):
     """
     Checking whether a given string is lisp-like.  
@@ -145,58 +177,76 @@ def is_lisplike_string(input_str):
     """
     # Parser must be able to parse without raising any exception
     try:
-        nested_list_repr = parser(input_str, noerr=False)
-        return True
-    except Exception:
-        return False
+        lisplike_repr = parse(input_str, noerr=False)
+    except NotLispLikeStringException:
+        lisplike_repr = None
+    return lisplike_repr is not None
 
 
-def is_lisplike_repr(value):
+# Almost surely obsolete. Delete in future versions
+# def is_subexpr_string(subexpr_string, expr_string):
+#     """
+#     Check if the first argument is a sub-expression of the second. The expressions are given 
+#     as strings.  
+#     :param subexpr_string: string  
+#     :param expr_string: string  
+#     :return: bool  
+#     """
+#     if not is_lisplike_string(subexpr_string):
+#         raise ValueError('The arguments need to be a lisp-like string: check first argument.')
+#     elif not is_lisplike_string(expr_string):
+#         raise ValueError('The arguments need to be a lisp-like string: check second argument.')
+#     return subexpr_string in expr_string
+
+
+def count_subexpr(subexpr_repr, expr_repr):
     """
-    Checking whether a value is a string or a nested list of strings.  
-    :param value: any  
+    Count the number of times the first argument occurs as a sub-expression of the second.  
+    :param subexpr_repr: is_lisplike  
+    :param expr_repr: is_lisplike  
     :return: bool  
     """
-    if isinstance(value, str):
+    if not is_lisplike(subexpr_repr):
+        raise NotLispLikeReprException('The arguments need to be a lisp-like representation: check first argument.')
+    elif not is_lisplike(expr_repr):
+        raise NotLispLikeReprException('The arguments need to be a lisp-like representation: check second argument.')
+    return _count_subexpr_aux(subexpr_repr, expr_repr)
+
+
+def _count_subexpr_aux(subexpr_repr, expr_repr):
+    # Check if it is already equal to the expression
+    count = 1 if subexpr_repr == expr_repr else 0
+    if isinstance(expr_repr, list):
+        # If the expression is an application (rather than a string which is an utterance), then 
+        # check for the number of occurrences among all its sub-expressions as well.
+        count = count + sum(_count_subexpr_aux(subexpr_repr, e) for e in expr_repr)
+    return count
+
+
+def is_subexpr(subexpr_repr, expr_repr):
+    """
+    Check if the first argument is a sub-expression of the second.  
+    :param subexpr_repr: is_lisplike  
+    :param expr_repr: is_lisplike  
+    :return: bool  
+    """
+    return count_subexpr(subexpr_repr, expr_repr) != 0
+
+
+def less_than(repr1, repr2):
+    """
+    Custom less-than for lisp-like representation. Utterances are smaller than application expressions, and all other 
+    orderings are fixed but arbitrary.  
+    :param repr1: is_lisplike  
+    :param repr2: is_lisplike  
+    :return: bool  
+    """
+    if not is_lisplike(repr1):
+        raise NotLispLikeReprException('The arguments need to be a lisp-like representation: check first argument.')
+    elif not is_lisplike(repr2):
+        raise NotLispLikeReprException('The arguments need to be a lisp-like representation: check second argument.')
+    if isinstance(repr1, str) and not isinstance(repr2, str):
         return True
-    if isinstance(value, list):
-        return all(is_lisplike_repr(v) for v in value)
     else:
-        return False
-
-
-def is_subexpr_string(subexpr_string, expr_string):
-    """
-    Check if the first argument is a sub-expression of the second. The expressions are given 
-    as strings.  
-    :param subexpr_string: string  
-    :param expr_string: string  
-    :return: bool  
-    """
-    if not is_lisplike_string(subexpr_string):
-        raise ValueError('The arguments need to be a lisp-like string: check first argument.')
-    elif not is_lisplike_string(expr_string):
-        raise ValueError('The arguments need to be a lisp-like string: check second argument.')
-    return subexpr_string in expr_string
-
-
-def is_subexpr_repr(subexpr_repr, expr_repr):
-    """
-    Check if the first argument is a sub-expression of the second. The expressions are in the form 
-    of the read in representations.  
-    :param subexpr_repr: is_lisplike_repr  
-    :param expr_repr: is_lisplike_repr  
-    :return: bool  
-    """
-    if not is_lisplike_repr(subexpr_repr):
-        raise ValueError('The arguments need to be a lisp-like representation: check first argument.')
-    elif not is_lisplike_repr(expr_repr):
-        raise ValueError('The arguments need to be a lisp-like representations: check second argument.')
-    return is_subexpr_repr_aux(subexpr_repr, expr_repr)
-
-
-def is_subexpr_repr_aux(subexpr_repr, expr_repr):
-    if isinstance(expr_repr, str):
-        return subexpr_repr == expr_repr
-    elif isinstance(expr_repr, list):
-        return subexpr_repr == expr_repr or any(is_subexpr_repr(subexpr_repr, e) for e in expr_repr)
+        # Both strings or both nested lists. Use built-in comparison
+        return repr1 < repr2
